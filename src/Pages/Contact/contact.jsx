bbe66770bfe24bcb1c3bf3f5088fc39281dafe90
carolyne-wanjiru <shirogithenduka@gmail.com>
2024-06-09 22:23:48 +0300

    add final touch ups

--- src/Pages/Contact/contact.jsx
+++ src/Pages/Contact/contact.jsx
@@ -66,16 +66,22 @@ const Contact = () => {
         </div>
         <div className="form-map">
           <div className="map-side">
-            <img className="map" src={Map}></img>
+            <iframe className="maps"
+              src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d15958.012317845742!2d37.158912!3d-0.7202532!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2ske!4v1717959865419!5m2!1sen!2ske"
+              width="400"
+              height="240"
+              allowfullscreen=""
+              loading="lazy"
+              referrerpolicy="no-referrer-when-downgrade"
+            ></iframe>
           </div>
           <div className="form-side">
-            <form>
-                <input type="text" placeholder="Full Name" required />
-                <input type="text" placeholder="Email Address" required />
-                <input type="text" placeholder="subject" required />
-                <input type="textarea" placeholder="message" required />
-                <button className="messagebtn">Send Message</button>
-             
+            <form className="form-input">
+              <input type="text" placeholder="Full Name" required />
+              <input type="text" placeholder="Email Address" required />
+              <input type="text" placeholder="subject" required />
+              <input type="textarea" placeholder="message" required />
+              <button className="messagebtn">Send Message</button>
             </form>
           </div>
         </div>
